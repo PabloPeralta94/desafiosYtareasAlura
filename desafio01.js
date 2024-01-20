c18ce837fb6565c0d77f2bddd990361dec1bd779
unknown <pablo.peralta94@gmail.com>
2024-01-19 21:19:53 -0300

    desafio 01 completado

---
+++ desafio01.js
@@ -0,0 +1,18 @@
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+let nombre = prompt("Ingresa tu nombre");
+let edad = prompt("Ingresa tu edad");
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+
+    let mensaje = "";
+    if (edad >= 18) {
+        mensaje = "¡Puedes obtener tu licencia de conducir!";
+    }
+    else {
+        mensaje = "no puedes obtener la licencia de conducior";
+    }
+
+alert(mensaje);
